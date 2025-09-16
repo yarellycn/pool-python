18d212ce887274f012d4bb5485ec972fb88b47a4
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2025-09-16 23:17:44 +0200

    j6'

--- POOL.J6/TASK2.3.py
+++ POOL.J6/TASK2.3.py
@@ -1,9 +1,27 @@
 import os
 
-cwd = os.getcwd() 
-print("Current working directory:", cwd)
-
 # os.chdir("path")
 
-print(os.listdir())       #List files and directory
+# (os.listdir())       #List files and directory
 
+
+# os.path.isfile(path): returns True if the path is a valid file
+# os.path.isdir(path): returns True if the path is a valid directory
+# os.path.exists(path) : returns True if the path is a valid file or directory
+
+def readdir(path, current_folder):
+    print(f"{current_folder}:")
+    directory = os.listdir(path)
+    directory = list(filter(lambda x: not x[0] == ".", directory))
+    for item in directory:
+        print(item, end= " ")
+    print("")
+    for item in directory:
+        if os.path.isdir(path + "/" + item):
+            print("")
+            readdir(path + "/" + item, current_folder + "/" + item)
+        
+
+
+cwd = os.getcwd()       #Gets current path
+readdir(cwd, ".")
